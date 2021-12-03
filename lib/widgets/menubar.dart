62f7ba40f2788eab1fc5b803e15d67f75527d5f1
potato0520 <potato20040520@gmail.com>
2021-12-03 22:29:20 +0900

    menu bar

--- lib/widgets/menubar.dart
+++ lib/widgets/menubar.dart
@@ -1,36 +1,35 @@
 import 'package:flutter/material.dart';
-import 'sidebar1.dart';
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
+
+  static const appTitle = 'Main';
+
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
+      title: appTitle,
+      home: MyHomePage(title: appTitle),
       debugShowCheckedModeBanner: false,
-      home: MyStatelessWidget(),
     );
   }
 }
 
-/// This is the stateless widget that the main application instantiates.
-class MyStatelessWidget extends StatelessWidget {
-  const MyStatelessWidget({Key? key}) : super(key: key);
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
+
+  final String title;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.white,
       appBar: AppBar(
+        backgroundColor: Colors.white,
+        iconTheme: IconThemeData(color: Colors.black),
         elevation: 0,
-        automaticallyImplyLeading: false,
         centerTitle: true,
-        leading: IconButton(
-          icon: Icon(Icons.menu),
-          onPressed: () => Scaffold.of(context).openDrawer(),
-          color: Colors.black,
-        ),
         title: Text(
-          'Main',
+          title,
           style:
               TextStyle(color: Colors.black, fontFamily: 'Chef', fontSize: 24),
         ),
@@ -40,13 +39,106 @@ class MyStatelessWidget extends StatelessWidget {
             icon: Icon(Icons.account_circle),
             onPressed: () => {},
             color: Colors.black,
-          ),
+          )
         ],
-        backgroundColor: Colors.white,
       ),
-      drawer: Drawer(
-        child: ListView(
-          padding: EdgeInsets.zero,
+      drawer: Container(
+        width: MediaQuery.of(context).size.width * 2 / 3,
+        child: Drawer(
+          child: ListView(
+            padding: EdgeInsets.zero,
+            children: <Widget>[
+              const DrawerHeader(
+                decoration: BoxDecoration(color: Colors.white),
+                child: Text('user info'),
+              ),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  SizedBox(
+                    width: 400,
+                    height: 60,
+                    child: TextButton.icon(
+                      icon: Icon(
+                        Icons.chat,
+                        size: 30,
+                        color: Colors.orange,
+                      ),
+                      label: Text(
+                        'New Post',
+                        style: TextStyle(color: Colors.black),
+                      ),
+                      onPressed: () {},
+                    ),
+                  ),
+                  SizedBox(
+                    width: 400,
+                    height: 60,
+                    child: TextButton.icon(
+                      icon: Icon(
+                        Icons.book,
+                        size: 30,
+                        color: Colors.orange,
+                      ),
+                      label: Text(
+                        'Shared Recipes',
+                        style: TextStyle(color: Colors.black),
+                      ),
+                      onPressed: () {},
+                    ),
+                  ),
+                  SizedBox(
+                    width: 400,
+                    height: 60,
+                    child: TextButton.icon(
+                      icon: Icon(
+                        Icons.money,
+                        size: 30,
+                        color: Colors.orange,
+                      ),
+                      label: Text(
+                        'Food Market',
+                        style: TextStyle(color: Colors.black),
+                      ),
+                      onPressed: () {},
+                    ),
+                  ),
+                  SizedBox(
+                    width: 400,
+                    height: 60,
+                    child: TextButton.icon(
+                      icon: Icon(
+                        Icons.class_,
+                        size: 30,
+                        color: Colors.orange,
+                      ),
+                      label: Text(
+                        'Cooking Class',
+                        style: TextStyle(color: Colors.black),
+                      ),
+                      onPressed: () {},
+                    ),
+                  ),
+                  SizedBox(
+                    width: 400,
+                    height: 60,
+                    child: TextButton.icon(
+                      icon: Icon(
+                        Icons.holiday_village,
+                        size: 30,
+                        color: Colors.orange,
+                      ),
+                      label: Text(
+                        'Village',
+                        style: TextStyle(color: Colors.black),
+                      ),
+                      onPressed: () {},
+                    ),
+                  ),
+                ],
+              )
+            ],
+          ),
         ),
       ),
     );
