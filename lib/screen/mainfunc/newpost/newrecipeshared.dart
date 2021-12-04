8bc534dd19fac0d571aa32c3e75ab95ca1f724c3
potato0520 <potato20040520@gmail.com>
2021-12-04 13:26:48 +0900

    Post New RecipeShare Page make

---
+++ lib/screen/mainfunc/newpost/newrecipeshared.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class NewRecipeShared extends StatelessWidget {
+  const NewRecipeShared({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
