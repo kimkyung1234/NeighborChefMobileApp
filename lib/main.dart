ab4b8b51f1fed79aa5bdbbcd6a5341f71e3640a7
potato0520 <potato20040520@gmail.com>
2021-12-04 13:28:26 +0900

    Main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:neighborchef/screen/startPage.dart';
+import 'package:neighborchef/screen/startpage.dart';
 
 void main() => runApp(const MyApp());
 
